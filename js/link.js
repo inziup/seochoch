a93e672b9696f594397165a80067ae25d48c6679
inziup <ginmall@naver.com>
2025-05-30 10:15:40 +0900

    Add files via upload

--- js/link.js
+++ js/link.js
@@ -1,11 +1,11 @@
 const linkList = [
 // 참여자등록 링크수정방법 a href="따옴표 안에 수정 링크를 넣으세요"//
-    { id: `linkList1`, content: `<a href="https://naver.me/FhfvD7lW"
+    { id: `linkList1`, content: `<a href="https://naver.me/G1mZcAYQ"
         target="_blank"><button type="button" class="button">참여자등록</button></a>
         <button type="button" class="button" onclick="goToNext00()">게임시작하기</button>` }, 
 
 // 만족도 링크 수정방법 a href="따옴표 안에 수정 링크를 넣으세요"//
-    { id: `linkList2`, content: `<a href="https://naver.me/5YSZrMYJ"
+    { id: `linkList2`, content: `<a href="https://naver.me/5gYPBURd"
         target="_blank"><button type="button" class="button">오늘의 소감</button></a>
         <p>게임을 마치는 소감을 적어주세요.<br>선물을 드려요.</p>` },
 ]
